417f664c5fcde469e3f0520711f9f91f224b6d0f
JesusMartinez1 <32658820+JesusMartinez1@users.noreply.github.com>
2017-10-24 13:58:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int i,j,A[3][3],B[3][3],C[3][3];
-int main()
- {
+int main(){
+	
+	srand(time(NULL));
   
-  printf("\t\nEste programa Suma dos matrices de 2x2\n");
-  printf("\n\tIngresa las entradas de la Matriz A\n\n");
-  for(i=1;i<=2;i++)
+  int num, num1;
+  printf("\t\nEste programa Suma dos matrices\n");
+  printf("De que tamaño quiere su matriz ?\n");
+  scanf("%d", &num);
+  //MATRIZ A
+  for(i=1;i<=num;i++)
    {
-	for(j=1;j<=2;j++)
-	 {
-	  printf("Da un valor para A[%d][%d]:  ",i,j);
-	  scanf("%i",&A[i][j]);
+	for(j=1;j<=num;j++){
+		A[i][j] = 1 + rand() % (10- 1);
+	  
 	 }
 }
 
-printf("\n\tIngresa las entradas de la Matriz B\n\n");
-  for(i=1;i<=2;i++)
+	//MATRIZ B
+  for(i=1;i<=num;i++)
    {
-	for(j=1;j<=2;j++)
-	 {
-	  printf("Da un valor para B[%d][%d]:  ",i,j);
-	  scanf("%i",&B[i][j]);
+	for(j=1;j<=num;j++){
+		B[i][j] = 1 + rand() % (10- 1);
 	 }
    }
    
    printf("\n\t\tEl resultado de la suma de ambas Matices es:\n\n");
-  for(i=1;i<=2;i++)
+  for(i=1;i<=num;i++)
    {
-	for(j=1;j<=2;j++)
+	for(j=1;j<=num;j++)
 	 {
 	  C[i][j]=A[i][j]+B[i][j];
 		  printf("\t\t\t    %d",C[i][j]);
